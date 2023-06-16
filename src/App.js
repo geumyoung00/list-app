e87cc180fc514df5674406cd180bb1f9f78e6737
stay <sihen54k@naver.com>
2023-06-17 01:05:08 +0900

    created props : date, title ,price

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { Component } from "react";
 import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
   return (
-    <div className="App" class="app">
-      <ExpenseItem></ExpenseItem>
+    <div className="App app">
+      <ExpenseItem date="2023년 6월 16일" title="New TV" price="₩ 1,000,000"></ExpenseItem>
+      <ExpenseItem date="2023년 6월 15일" title="Note" price="₩ 3,000"></ExpenseItem>
     </div>
   );
 }
+
 
 export default App;
--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,11 +1,12 @@
+import React, { Component } from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div class="itembox">
-      <p class="txt date"><span>2023년</span> 6월 10일</p>
-      <p class="txt name">New TV</p>
-      <p class="txt price">₩ 20,000</p>
+    <div className="expense__item">
+      <p className="txt date">{props.date}</p>
+      <p className="txt title">{props.title}</p>
+      <p className="txt price">{props.price}</p>
     </div>
   );
 }
