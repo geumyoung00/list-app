a87482923d45fb0bb0b81d924b57cb3d989cf77f
stay <sihen54k@naver.com>
2023-06-29 19:18:25 +0900

    merged useState

--- src/component/AddItem.js
+++ src/component/AddItem.js
@@ -5,44 +5,50 @@ import "./component.css";
 
 const AddItem = (props) => {
     
+    // state 통합
+    const [newItem, setNewItem] = useState({title:'IceCream', price:100, purchaseDate:'01.01.2023'});
+
     const [title, setTitle] = useState('Ice Cream');
     const [price, setPrice] = useState(100);
     const [purchaseDate, setPurchaseDate] = useState('01.01.2023');
 
-    const handleChangeTitle = (event) => {``
+    const handleChangeTitle = (event) => {
         // console.log('event',event);
         // console.log('event.target.value',event.target);
 
-        setTitle(event.target.value);
+        // setTitle(event.target.value);
+        setNewItem({...newItem, title:event.target.value});
 
     };
 
     const handleChangePrice = (event) => {
-        setPrice(event.target.value);
+
+        setNewItem({...newItem, price:event.target.value});
     };
 
     const handleChangePurchaseDate = (event) => {
-        setPurchaseDate(event.target.value);
+
+        setNewItem({...newItem, purchaseDate:event.target.value});
     };
 
 
-    console.log(title, price, purchaseDate);
+    console.log(newItem);
 
     return (
     <form className="input_box">
       <ul className="input">
         <li className="ip_title">
           <span>품목</span>
-          <input type="text" id="title" defaultValue={title} onChange={handleChangeTitle} />
+          <input type="text" id="title" defaultValue={newItem.title} onChange={handleChangeTitle} />
         </li>
         <li className="ip_price">
           <span>가격</span>
-          <input type="number" id="price" defaultValue={price} onChange={handleChangePrice} />
+          <input type="number" id="price" defaultValue={newItem.price} onChange={handleChangePrice} />
         </li>
         <li className="ip_date">
           <span>날짜</span>
           <div className="datePicker">
-            <input type="text" id="date" defaultValue={purchaseDate} onChange={handleChangePurchaseDate} />
+            <input type="text" id="date" defaultValue={newItem.purchaseDate} onChange={handleChangePurchaseDate} />
             <i className="ico_calander"></i>
           </div>
         </li>
