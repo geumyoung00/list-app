5a5a927ddf403137b8c708772e1b6fb0211fcdcc
Younji Woo <younjiwoo@gmail.com>
2023-06-30 17:10:55 +0900

    Removed state managing part in AddItem.js

--- src/component/AddItem.js
+++ src/component/AddItem.js
@@ -4,51 +4,22 @@ import "./component.css";
 // 이벤트리스너
 
 const AddItem = (props) => {
-    
-    // state 통합
-    const [newItem, setNewItem] = useState({title:'IceCream', price:100, purchaseDate:'01.01.2023'});
-
-    const [title, setTitle] = useState('Ice Cream');
-    const [price, setPrice] = useState(100);
-    const [purchaseDate, setPurchaseDate] = useState('01.01.2023');
-
-    const handleChangeTitle = (event) => {
-        // console.log('event',event);
-        // console.log('event.target.value',event.target);
-
-        // setTitle(event.target.value);
-        setNewItem({...newItem, title:event.target.value});
-
-    };
-
-    const handleChangePrice = (event) => {
-
-        setNewItem({...newItem, price:event.target.value});
-    };
-
-    const handleChangePurchaseDate = (event) => {
-
-        setNewItem({...newItem, purchaseDate:event.target.value});
-    };
-
-
-    console.log(newItem);
 
     return (
     <form className="input_box">
       <ul className="input">
         <li className="ip_title">
           <span>품목</span>
-          <input type="text" id="title" defaultValue={newItem.title} onChange={handleChangeTitle} />
+          <input type="text" id="title" />
         </li>
         <li className="ip_price">
           <span>가격</span>
-          <input type="number" id="price" defaultValue={newItem.price} onChange={handleChangePrice} />
+          <input type="number" id="price" />
         </li>
         <li className="ip_date">
           <span>날짜</span>
           <div className="datePicker">
-            <input type="text" id="date" defaultValue={newItem.purchaseDate} onChange={(event)=>handleChangePurchaseDate(event)} />
+            <input type="text" id="date" />
             <i className="ico_calander"></i>
           </div>
         </li>
